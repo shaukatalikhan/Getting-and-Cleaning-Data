562b951cf3f30445c6bf443e3c12f91cb23cccd2
shaukatalikhan <sylar_ali@yahoo.com>
2015-02-11 22:55:51 +0500

    course project files successfully uploaded

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 # Source of data for this project: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
-# This R script does the following:
+# This R script does the following
 
 # 1. Merges the training and the test sets to create one data set.
 
